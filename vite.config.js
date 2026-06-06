d4d947064195562e8ce203ab75205ac2a66e6665
Ulrich IDOHOU <ulrichidohou5@gmail.com>
2026-06-06 08:50:52 +0100

    Configure GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // Configuration Vite : React pour l'interface et Tailwind CSS pour le style.
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/portfolio-personel/",
 });
