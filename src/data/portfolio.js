4fd677bd0fbb533f58eadf4c5c15e4bc58fa2c89
Ulrich IDOHOU <ulrichidohou5@gmail.com>
2026-07-04 11:46:44 +0100

    fix: rename project to UNIONE

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -98,7 +98,7 @@ export const projects = [
     year: "2026",
   },
   {
-    name: "UniOne",
+    name: "UNIONE",
     description:
       "Assistant USSD intelligent pour le Bénin regroupant les actions courantes pour MTN, MOOV et Celtiis : solde, transfert, achat de crédit, forfaits et assistance.",
     technologies: ["Flutter", "Dart", "Material Design 3"],
